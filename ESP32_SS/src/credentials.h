1783df053479ca19adba35b4bbc0b1ba15883711
gabrmour <57685745+gabrmour@users.noreply.github.com>
2019-11-13 14:08:46 -0800

    Update credentials.h

--- ESP32_SS/src/credentials.h
+++ ESP32_SS/src/credentials.h
@@ -1,13 +1,9 @@
 #define FIREBASE_HOST "espSS.firebaseio.com" 
 #define FIREBASE_AUTH "sjPs17K5ixPBqPWhyeYsEHQCgdXLWfkarxEzSSgO" 
-#define WIFI_SSID "BarleyTea"
-#define WIFI_PASSWORD "S1mark47Sle3p3r"
 
 #define FIREBASE_HOST "test-d83d0.firebaseio.com"
 #define FIREBASE_WEB_API "AIzaSyD2O4MgY6uhj9pBiXueUpEchtVqLDYj3xs" 
 #define FIREBASE_AUTH "xjh8oFC7o3HEWIUnJ7G3qmfmSVEPKJJd5w3ZvMi1" 
-#define WIFI_SSID "SFState"
-#define WIFI_PASSWORD "$h2i0neTAIyu"
 
 #include <Arduino.h>
 #include <HX711.h>
@@ -17,10 +13,12 @@
 
 #define FIREBASE_HOST "test-d83d0.firebaseio.com"
 #define FIREBASE_AUTH "xjh8oFC7o3HEWIUnJ7G3qmfmSVEPKJJd5w3ZvMi1"
-#define WIFI_SSID     "SFState"
-#define WIFI_PASSWORD "$h2i0neTAIyu"
+#define WIFI_SSID     ""
+#define WIFI_PASSWORD ""
 
 #define SETUP_PIN 4 //set up a user calibration pin
 
 #define DOUT  25  // Scale data pin
 #define CLK   26  // Scale clock pin
+#define 
+#define 
