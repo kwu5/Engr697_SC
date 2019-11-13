2c879b37cbe93140fdf247c410a1e28e1e5b3c9b
gabrmour <57685745+gabrmour@users.noreply.github.com>
2019-11-13 14:09:17 -0800

    Update WiFiConfig.h

--- ESP32_SS/src/WiFiConfig.h
+++ ESP32_SS/src/WiFiConfig.h
@@ -1,7 +1,3 @@
-/* const char* WIFI_SSID = "SFState";
-const char* WIFI_PASSWORD = "$h2i0neTAIyu";
-const char* WIFI_USER = "909375316"; */
-
 #define  WIFI_SSID "Daiichiru"
 #define  WIFI_PASS "bab7fad623a6"
 const char* SSID = "Daiichiru";
