b93450835946371f470d7ab1037ecd4207fca7ed
gabrmour <57685745+gabrmour@users.noreply.github.com>
2019-12-01 19:27:20 -0800

    Update main.cpp

--- ESP32_SS/src/main.cpp
+++ ESP32_SS/src/main.cpp
@@ -2,27 +2,29 @@
 #include <HX711.h>
 #include <WiFi.h>
 #include <FirebaseESP32.h>
-#include "WiFiConfig.h"
 
-#define FIREBASE_HOST "test-d83d0.firebaseio.com"
-#define FIREBASE_AUTH "xjh8oFC7o3HEWIUnJ7G3qmfmSVEPKJJd5w3ZvMi1"
-//#define FIREBASE_HOST "espSS.firebaseio.com" 
-//#define FIREBASE_AUTH "sjPs17K5ixPBqPWhyeYsEHQCgdXLWfkarxEzSSgO" 
+#define  SSID "KINAGO 8270"
+#define  PASS "1C2t8(77"
+
+#define FIREBASE_HOST "smartscale697.firebaseio.com" 
+#define FIREBASE_AUTH "sjPs17K5ixPBqPWhyeYsEHQCgdXLWfkarxEzSSgO" 
 
 #define DOUT  25  // Scale data pin
 #define CLK   26  // Scale clock pin
-#define Threshold 40 /* Greater the value, more the sensitivity */
+#define Threshold 40
 touch_pad_t touchPin;
 
 class HX711 scale;
 float calF = -12450; // Calibration Factor -7050, -12450.00,
-int n;
-FirebaseData DT;
+
+FirebaseData UserId;
 FirebaseData WEIGHT;
 
 void callback() {
 Serial.println("touchpad wake");
-scale.power_up(); //power up scale
+Serial.println(touchRead(15));
+Serial.println(touchRead(4));
+scale.power_up();
 }
 
 void setup() {
@@ -30,8 +32,8 @@ void setup() {
   delay(1000); //Take some time to open up the Serial Monitor
 
   // Setup interrupt on (GPIO04 and GPIO15)
-  touchAttachInterrupt(15, callback, Threshold);
-  touchAttachInterrupt(4, callback, Threshold);
+  touchAttachInterrupt(15, callback, Threshold);  // sleepwake center capacitive button?
+  touchAttachInterrupt( 4, callback, Threshold);
 
   // Configure Touchpad as wakeup source
   esp_sleep_enable_touchpad_wakeup();
@@ -44,22 +46,22 @@ void setup() {
   Serial.print("Zero factor: "); // Removes need for init tare
   Serial.println(zero_factor);
 
-   // Wifi connect 
+  // Wifi connect 
   WiFi.begin(SSID, PASS); 
   Serial.print("Connecting to Wi-Fi \n"); 
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print("."); 
     delay(500); 
     }
-  
+ 
   Serial.println(); 
   Serial.print("Connected to IP: "); 
   Serial.println(WiFi.localIP()); 
    
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
-  String path = "/ESP_SS/WEIGHT_RAW";
-}
+  Firebase.getShallowData(UserId, "user");
+  }
 
 void loop() {
   scale.set_scale(calF); 
@@ -71,12 +73,12 @@ void loop() {
   Serial.print(calF);
   Serial.println();
   
-  Firebase.pushTimestamp(DT, "path");
-  Firebase.pushFloat(WEIGHT, "path", weight);
-  delay(120000);
+  Firebase.setFloat(WEIGHT, "user/user_1/weight", weight);
+
+  // Sleep protocol
+  delay(30000);
   scale.power_down();
-    //Go to sleep
-  Serial.println("Going to sleep now");
+  Serial.println("Scale entering sleep mode");
   esp_deep_sleep_start();
-  Serial.println("This will never be printed");
+  Serial.println("Verify sleep");
 }
