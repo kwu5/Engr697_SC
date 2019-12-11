7770788d663c619ccef9d4c7304a2479419e29de
gabrmour <57685745+gabrmour@users.noreply.github.com>
2019-12-11 14:55:46 -0800

    Update main.cpp

--- ESP32_SS/src/main.cpp
+++ ESP32_SS/src/main.cpp
@@ -11,7 +11,10 @@
 /* Firebase authorization & init*/
 #define FIREBASE_HOST "smartscale697.firebaseio.com" 
 #define FIREBASE_AUTH "sjPs17K5ixPBqPWhyeYsEHQCgdXLWfkarxEzSSgO" 
-FirebaseData TIMEST;
+// #define FIREBASE_HOST "test-d83d0.firebaseio.com"
+// #define FIREBASE_AUTH "xjh8oFC7o3HEWIUnJ7G3qmfmSVEPKJJd5w3ZvMi1" 
+
+FirebaseData TIME;
 FirebaseData WEIGHT;
 
 /* NTP server credentials */
@@ -25,37 +28,21 @@ char timestamp[50];
 #define CLK   4  // Scale clock pin
 #define CP    14  //control pin/reset
 #define Threshold 40
-touch_pad_t touchpad;
 
+/* Wake interrupt */
+void wakeup(){
+  esp_restart();
+}
 /* HX711 call */
 class HX711 scale;
 float calF = -22185; // Calibration Factor
-
-/* wake from deep sleep */
-void callback(){
-  Serial.println("touch pin wake");
-  scale.power_up();
-}
-
-/* Get timestamp from server */
-void printLocalTime() {
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)) {
-    Serial.println("Failed to obtain time");
-    return;
-  }
-  Serial.println(&timeinfo, "%A, %B %d %Y %l:%M:%S %p");
-  strftime(timestamp, 50, "%B %d, %Y %l:%M:%S %p", &timeinfo);
-  Firebase.pushString(TIMEST, "user/user1/scale/TIME", timestamp);
-  Serial.printf(timestamp);
-}
 
 void setup() {
   Serial.begin(115200);
   // delay(1000); //Take some time to open up the Serial Monitor
  
  // Configure wakeup source
-  touchAttachInterrupt(CP, callback, Threshold);
+  touchAttachInterrupt(CP, wakeup, Threshold);
   esp_sleep_enable_touchpad_wakeup();
 
   // Scale startup
@@ -71,6 +58,7 @@ void setup() {
   Serial.print("Connecting to Wi-Fi \n"); 
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print("."); 
+    digitalWrite(32, ~ digitalRead(32));
     delay(500); 
     }
  
@@ -78,33 +66,44 @@ void setup() {
   Serial.print("Connected to IP: "); 
   Serial.println(WiFi.localIP()); 
 
-  // init and get the time
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
-
   // Firebase connect   
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
-  }
+
+  // init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  struct tm timeinfo;
+    if(!getLocalTime(&timeinfo)) {
+      Serial.println("Failed to obtain time");
+      return;
+    }
+  strftime(timestamp, 50, "%B %d, %Y %l:%M:%S %p", &timeinfo);
+  Serial.println(timestamp);
+  Firebase.setString(TIME, "ESP_SS/TIMESET", timestamp);
+  
+}
 
 void loop() {
-  delay(10000);
-  // pinMode(2, true);
+  digitalWrite(32, HIGH);
+  delay(5000);
   scale.set_scale(calF); 
+  Serial.print("Calibration factor: ");
+  Serial.println(calF);
   Serial.print("Reading: ");
   float weight = scale.get_units(10);
   Serial.print(weight);
-  Serial.print(" kg");
-  Serial.print(" calibration factor: ");
-  Serial.print(calF);
-  Serial.println();
-  
-  Firebase.pushFloat(WEIGHT, "user/user1/scale/WEIGHT", weight);
+  Serial.println(" kg");  
+  Firebase.setDouble(WEIGHT, "ESP_SS/WEIGHT", weight);
+
 
   // Sleep protocol
   delay(30000);
+  digitalWrite(32, LOW);
   scale.power_down();
   Serial.println("Scale entering sleep mode");
   esp_deep_sleep_start();
   Serial.println("Verify sleep");
-}
+
+/* wake from deep sleep */
+  Serial.println("touch pin wake");
+  scale.power_up();
